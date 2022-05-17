127fc71a2efecf1c83517c465218ebdf9084fda5
Parasgharu <105363965+Parasgharu@users.noreply.github.com>
2022-05-17 17:12:06 +0530

    Add files via upload
    servlet

--- Servlets & JSP/05_Employee_JDBC/src/main/java/com/mindgate/controller/AddEmployeeServlet.java
+++ Servlets & JSP/05_Employee_JDBC/src/main/java/com/mindgate/controller/AddEmployeeServlet.java
@@ -2,12 +2,14 @@ package com.mindgate.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.mindgate.DAO.EmployeeDAO;
 import com.mindgate.pojo.Employee;
@@ -35,9 +37,13 @@ public class AddEmployeeServlet extends HttpServlet {
 		response.setContentType("text/html");
 		
 		if (result) {
-			out.println("<br>Record Added Successfully");
-			out.println("<br><a href =employeehome.html> Home Page </a>");
-		} else {
+//			out.println("<br>Record Added Successfully");
+//			out.println("<br><a href =employeehome.html> Home Page </a>");
+//			Set<Employee> allEmployees=employeeDAO.getAllEmployee();
+//			HttpSession session= request.getSession();
+//			session.setAttribute("data", allEmployees);
+			response.sendRedirect("GetAllEmployeeServlet");
+		} else {  
 			out.println("<br>Failed to Add Record");
 			out.println("<br><a href =employeehome.html> Home Page </a>");
 		}
